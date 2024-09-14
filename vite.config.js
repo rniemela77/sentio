6a9a57d1d71ee951cc75389210c32d282e057653
rniemela77 <rvniemela@hotmail.com>
2024-09-13 19:23:20 -0700

    gh action try again

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,7 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // Define configuration based on the environment mode
-export default defineConfig(({ command, mode }) => {
-  // Determine the base path depending on the environment
-  const base = mode === 'production' ? '/sentio/' : '/';
-
-  return {
-    base,  // Base path for GitHub Pages
+export default defineConfig({
+    base: process.env.NODE_ENV === 'production' ? '/sentio/' : '/',
     plugins: [svelte()],
-  };
 });
