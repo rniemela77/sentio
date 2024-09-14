1bcf122232fcdfd7b0b45ed1cbd519580538fd7c
rniemela77 <rvniemela@hotmail.com>
2024-09-13 18:26:51 -0700

    change path in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-export default defineConfig({
-  base: '/sentio/',
-  plugins: [svelte()],
+// Define configuration based on the environment mode
+export default defineConfig(({ command, mode }) => {
+  // Determine the base path depending on the environment
+  const base = mode === 'production' ? '/sentio/' : '/';
+
+  return {
+    base,  // Base path for GitHub Pages
+    plugins: [svelte()],
+  };
 });
